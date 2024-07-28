9a2fdc744034a54a21e581e33576df8240baa14e
Vinicius Cruz <vinaocruz@gmail.com>
2024-07-28 13:49:18 -0300

    chore: definir conexão nativa com pgsql

--- initdb/create-table.sql
+++ initdb/create-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE public.negotiations (
 	id SERIAL NOT NULL,
 	closedat VARCHAR NOT NULL,
-	transationat DATE NOT NULL,
+	transationat DATE NULL,
 	ticketcode VARCHAR NOT NULL,
 	price FLOAT4 NOT NULL,
 	quantity INTEGER NOT NULL,
