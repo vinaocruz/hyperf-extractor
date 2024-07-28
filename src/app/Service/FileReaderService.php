3b5bdb4de3e8ff5e8d2e6aed2e60a86381e8292e
Vinicius Cruz <vinaocruz@gmail.com>
2024-07-28 13:50:40 -0300

    feat: enviar dados como string para inserção

--- src/app/Service/FileReaderService.php
+++ src/app/Service/FileReaderService.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace App\Service;
 
-use Domain\Service\ReaderServiceInterface;
+use App\Model\Negotiation;
+use App\Domain\Service\ReaderServiceInterface;
 use League\Flysystem\Filesystem;
 use Swoole\Coroutine\Channel;
 use Throwable;
@@ -37,7 +38,24 @@ class FileReaderService implements ReaderServiceInterface
 
     private function parseLine(string $line): string
     {
-        //TODO parse line
-        return $line;
+        $part = explode(';', $line);
+
+        return sprintf(
+            '%s;%s;%s;%s;%s',
+            $part[5],
+            $part[8] ?? '',
+            $part[1],
+            (float)$part[3],
+            (int)$part[4]
+        );
+
+        // $negotiation = new Negotiation();
+        // $negotiation->closedat = $part[5];
+        // $negotiation->transationat = $part[8] ?? null;
+        // $negotiation->ticketcode = $part[1];
+        // $negotiation->price = (float) $part[3];
+        // $negotiation->quantity = (int) $part[4];
+
+        // return $negotiation;
     }
 }
